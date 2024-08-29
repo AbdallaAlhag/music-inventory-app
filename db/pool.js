f9bc5ff2104aaa522debc9f5ed64625cee6e3507
Abdalla Alhag <alhagabdalla@gmail.com>
2024-08-29 10:53:22 -0700

    set up database and tables with basic dp functionaility such as populate, pool, and query with .env page

---
+++ db/pool.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+require('dotenv').config()
+
+// All of the following properties should be read from environment variables
+// We're hardcoding them here for simplicity
+// module.exports = new Pool({
+//   host: "localhost", // or wherever the db is hosted
+//   user: "alhag",
+//   database: "music_inventory",
+//   password: "Alshabeek42!",
+//   port: 5432 // The default port
+// });
+
+module.exports = new Pool({
+  connectionString: process.env.CONNECTIONSTRING,
+});
