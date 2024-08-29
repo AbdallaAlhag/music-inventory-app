024f2acacfad7f9bf6e174c41844ed2f6cb25aa2
Abdalla Alhag <alhagabdalla@gmail.com>
2024-08-29 11:12:56 -0700

    Set up views

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// middleware for static files
+app.use(express.static('public'));
+
+
+// app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => {
+    res.render("index", { title: "Index!" });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
